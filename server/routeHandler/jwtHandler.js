2c522830db89748eac3af102b7de9a71ba6a262d
Hasan Mt <hasan.2mahmud22@gmail.com>
2023-09-08 00:42:16 +0600

    server auth modification using jwt token for authorization and secure important routes using admin verification

--- server/routeHandler/jwtHandler.js
+++ server/routeHandler/jwtHandler.js
@@ -1,25 +1,15 @@
 const express = require("express");
+const jwt = require('jsonwebtoken');
 const router = express.Router();
-const { ObjectId } = require("mongodb");
-
-const {  usersCollections } = require("../collections/collections");
-
 
 try {
   router.post("/", async (req, res) => {
-    
-    const email = req.query.email;
-            const query = { email: email };
-            const user = await usersCollections.findOne(query);
-            console.log(user)
-            if (user) {
-                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
-                expiresIn: "50d",
-                });
-                return res.send({ token: token });
-            }
-            res.status(403).send({ token: "" });
-            });  
+    const user = req.body;
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: '100d' })
+    console.log(token)
+    res.send({ token })  
+ });  
   } catch (error) {
     res.status(404).send({
       error: error.message
--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const app = require('./app/app')
+
 const port =process.env.PORT || 5000
+
 
 app.listen(port, () => console.log(`99 pro server listening on ${port}`))
